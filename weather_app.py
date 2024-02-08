d23818d34140bdf0424a4436e77a477fd81aae20
Hasan Furkan Duzman <114804692+Duzman46@users.noreply.github.com>
2024-02-08 19:25:03 +0300

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -6,7 +6,7 @@ api_key = "8141ab941bbebbbf4d28d6621ca902f6"
 
 def get_weather(city):
     params = {'q':city,'appid':api_key,'lang':'tr'}
-    data = requests.get(url,params=params).json()
+    data = requests.get(url,params).json()
     if data:
         city = data['name'].capitalize()
         country  = data['sys']['country']
@@ -23,26 +23,32 @@ def main():
         condition_label['text'] = weather[3]
 
 
-
 screen = tkinter.Tk()
-screen.geometry("350x550")
+screen.geometry("450x550")
 screen.config(bg="light pink")
 screen.title("Weather App")
+
+title_message = tkinter.Label(screen,text="Enter Country or City")
+title_message.config(bg="light pink",font=("arial",30))
+title_message.pack()
 
 search_entry = tkinter.Entry(screen)
 search_entry.pack(fill="both",ipady=10,padx=18,pady=5)
 search_entry.focus()
 
 search_button = tkinter.Button(screen,text="Search",font=("arial,15"),command=main)
-search_button.pack(fill="both",ipady=10,padx=20)
+search_button.pack()
 
 location_label = tkinter.Label(screen,font=('arial',40))
-location_label.pack()
+location_label.config(bg="light pink")
+location_label.pack(fill="both",ipady=10,padx=20)
 
 temp_label = tkinter.Label(screen,font=('arial',50,'bold'))
+temp_label.config(bg="light pink")
 temp_label.pack()
 
 condition_label = tkinter.Label(screen,font=('arial',20))
+condition_label.config(bg="light pink")
 condition_label.pack()
 
 tkinter.mainloop()
